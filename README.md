09553378c2cb9345f24bdc79e13775419d64c2d3
p0ntan <pontus.akerberg@hotmail.com>
2023-11-18 20:09:56 +0100

    Update bash-script and add docker-compose

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 Denna strukturen kan hållas i varje repo som gör det smidigt att sätta ihop och starta tillsammans i systemet.
 
-I rooten för varje repo ligger en README, ett bash-skript och en Dockerfile. Resten av filerna ligger sedan i en egen mapp, namnet är egentligen orelevant men förslagsvis src, app, root eller liknande. Det är sedan i den mappen man lägger själva applikationen och all kod, troligen det som tidigare varit rooten i andra repon man gjort.
+I rooten för varje repo ligger minst en README, ett bash-skript och en Dockerfile. Resten av filerna ligger sedan i en egen mapp, namnet är egentligen orelevant men förslagsvis src, app, root eller liknande. Det är sedan i den mappen man lägger själva applikationen och all kod, troligen det som tidigare varit rooten i andra repon man gjort.
 
 När man jobbar med sitt repo så bygger man det via docker och jobbar i containern med alla filer tillagda som volymer, då kan man vara säker på att alla kommer ha samma miljö när de sedan ska kopplas ihop. Imagen behöver alltså inte sparas och/eller laddas upp i utvecklingsfasen. Förslagsvis så använder man ett bash-skript som sköter detta åt en då kan man i huvudrepot sedan använda i stora drag samma uppsättning för att koppla ihop allt. Det blir även enklare för andra att bara köra skriptet för att köra repot lokalt.
+
+I detta repo används också docker-compose för att enklare sätta upp repot.
+
+```
+# För att starta containern
+./run.bash up
+
+# FÖr att stänga containern, och ta bort imagen som byggs
+./run.bash down
+```
---
+++ run.bash
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+#
+# Put code here to build and setup the repo
+#
+
+#!/usr/bin/env bash
+#
+# Bash script for working with routes on server
+#
+# Exit values:
+#  0 on success
+#  1 on failure
+#
+
+# Name of the script
+SCRIPT=$( basename "$0" )
+
+# Current version
+VERSION="1.0.0"
+
+#
+# Message to display for usage and help.
+#
+function usage
+{
+    local txt=(
+""
+"Script $SCRIPT is used to work with log-data."
+"Usage: $SCRIPT [options] <command> [arguments]"
+""
+"Commands:"
+""
+"   up                   Start the container."
+"   down                 Shut down the container."
+""
+""
+"Options:"
+""
+"   -h, --help      Display the menu"
+"   -v, --version   Display the current version"
+""
+    )
+
+    printf "%s\\n" "${txt[@]}"
+}
+
+
+#
+# Message to display when bad usage.
+#
+function badUsage
+{
+    local message="$1"
+    local txt=(
+"For an overview of the command, execute:"
+"$SCRIPT -h, --help"
+    )
+
+    [[ -n $message ]] && printf "%s\\n" "$message"
+
+    printf "%s\\n" "${txt[@]}"
+}
+
+
+#
+# Message to display for version.
+#
+function version
+{
+    local txt=(
+"$SCRIPT version $VERSION"
+    )
+
+    printf "%s\\n" "${txt[@]}"
+}
+
+#
+# Function to start the container
+#
+function app-up
+{
+    docker-compose up -d --build
+}
+
+#
+# Function to shut down the container
+#
+function app-down
+{
+    # Close the container
+    docker-compose down -v
+
+    # Remove the image
+    docker rmi simple-map
+
+}
+
+#
+# Process options
+#
+function main
+{
+    while (( $# ))
+    do
+        case "$1" in
+
+            --help | -h)
+                usage
+                exit 0
+            ;;
+
+            --version | -v)
+                version
+                exit 0
+            ;;
+
+            up            \
+            | down)
+                command="$1"
+                shift
+                app-"$command" "$@"
+                exit 0
+            ;;
+
+            *)
+                badUsage "Option/command not recognized."
+                exit 1
+            ;;
+
+        esac
+    done
+
+    badUsage
+    exit 1
+}
+
+main "$@"
